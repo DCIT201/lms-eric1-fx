e27f3604232dfa03b8c9069c27e40235e26835c7
Eric Manu <ericmanu670@gmail.com>
2024-12-19 12:15:13 +0100

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,4 +1,17 @@
-public class Book {
+package org.example;
+
+public record Book(String title, String author, int yearPublished) {
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return  title;
+    }
 
     // This is your working class.
     
